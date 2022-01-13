d1830ff40baadd39728f3573c753d912ee830590
ZeaCeR#5641 <36286877+hirusha-adi@users.noreply.github.com>
2022-01-13 08:27:42 +0530

    bot activity

--- nsfwbot/database/get_main.py
+++ nsfwbot/database/get_main.py
@@ -14,3 +14,4 @@ class BotMainDB:
     BOT_CREATOR_NAME = embed["BOT_CREATOR_NAME"]
     BOT_VERSION = embed["BOT_VERSION"]
     DEV_AND_OWNERS = embed["DEV_AND_OWNERS"]
+    BOT_ACTIVITY = embed["BOT_ACTIVITY"]
